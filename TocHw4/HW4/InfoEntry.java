1e906d6559d3bff1b826af44ec6da672f9111f30
Eric Wu <erickitten@gmail.com>
2014-06-26 11:52:37 +0800

    fix indent

--- TocHw4/HW4/InfoEntry.java
+++ TocHw4/HW4/InfoEntry.java
@@ -2,40 +2,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-	public class InfoEntry{
-		private int maxPrice,minPrice;
-		private List<String> distinctMonth;
-		
-		InfoEntry(int initPrice,String initDate){
-			maxPrice = minPrice = initPrice;
-			distinctMonth = new ArrayList<String>();
-			distinctMonth.add(initDate);
-		}
-		
-		public void updatePrice(int p){
-			if(p > maxPrice){
-				maxPrice = p;
-			}
-			if(p < minPrice){
-				minPrice = p;
-			}
-		}
-		
-		public void updateDistinctMonth(String date){
-			if(!distinctMonth.contains(date)){
-				distinctMonth.add(date);
-			}
-		}
+public class InfoEntry{
+	private int maxPrice,minPrice;
+	private List<String> distinctMonth;
 
-		public int getMaxPrice() {
-			return maxPrice;
-		}
+	InfoEntry(int initPrice,String initDate){
+		maxPrice = minPrice = initPrice;
+		distinctMonth = new ArrayList<String>();
+		distinctMonth.add(initDate);
+	}
 
-		public int getMinPrice() {
-			return minPrice;
+	public void updatePrice(int p){
+		if(p > maxPrice){
+			maxPrice = p;
 		}
-		
-		public int getNumberOfDistinctMonth(){
-			return distinctMonth.size();
+		if(p < minPrice){
+			minPrice = p;
 		}
 	}
+
+	public void updateDistinctMonth(String date){
+		if(!distinctMonth.contains(date)){
+			distinctMonth.add(date);
+		}
+	}
+
+	public int getMaxPrice() {
+		return maxPrice;
+	}
+
+	public int getMinPrice() {
+		return minPrice;
+	}
+
+	public int getNumberOfDistinctMonth(){
+		return distinctMonth.size();
+	}
+}
